502225eb46828ea6a4dbfab91d6fc22b01230717
sanikashelake49 <shelakesanika49@gmail.com>
2026-09-21 23:24:34 +0530

    Update DashboardPanel.java

--- E_Bill-Spilleter/src/com/ebillsplitter/ui/DashboardPanel.java
+++ E_Bill-Spilleter/src/com/ebillsplitter/ui/DashboardPanel.java
@@ -2,7 +2,7 @@ package com.ebillsplitter.ui;
 
 import java.awt.BorderLayout;
 import java.beans.Statement;
-import java.sql.Connection; 
+import java.sql.Connection;  
 import java.sql.*;
 
 import javax.swing.*;
